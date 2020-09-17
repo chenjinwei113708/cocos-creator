40bdba9b25aee547b927c42b626b01025d0a5334
readdan <435706956@qq.com>
2020-09-17 16:49:06 +0800

    空项目：更新场景

---
+++ fruitClash_case1/assets/Script/Views/GuideView.js
@@ -0,0 +1,66 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+import { CELL_WIDTH, CELL_HEIGHT, GRID_PIXEL_WIDTH, GRID_PIXEL_HEIGHT } from '../Model/ConstValue';
+
+/**
+ * 这个脚本是用来播放引导动作的
+ */
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        modal:cc.Node,
+        hand: cc.Node,
+    },
+    // LIFE-CYCLE CALLBACKS:
+    onLoad () { 
+    },
+
+    setGameController (gamecontroller) {
+        this.gamecontroller = gamecontroller;
+    },
+    // start () {},
+    /**展示欢迎页面 */
+    showWelcomePage(){
+        this.modal.active = true;
+        this.modal.runAction(cc.sequence(
+            cc.callFunc(() => {
+                this.modal.getChildByName('startPage').active = true;
+            }),
+            cc.fadeIn(.3)
+        ));
+    },
+    /**开始游戏的画面 */
+    startGame(){
+        this.modal.runAction(cc.sequence(
+            cc.fadeOut(.2),
+            cc.callFunc(() => {
+                this.modal.active = false;
+                this.modal.getChildByName('startPage').active = false;
+            })
+        ));
+    },
+    /**展示结束页面，并引导下载 */
+    showEndPage(){
+        //播放结束音乐
+        // if (isAudioEnabled) cc.audioEngine.playEffect(this.endingMusic, false, 2);
+        this.modal.active = true;
+        this.modal.runAction(cc.sequence(
+            cc.delayTime(1),
+            cc.callFunc(() => {
+                this.modal.getChildByName('endPage').active = true;
+            }),
+            cc.fadeIn(.5),
+        ));
+    },
+
+    
+});
