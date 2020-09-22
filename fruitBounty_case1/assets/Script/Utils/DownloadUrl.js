c057806b5cdbcd64dfd57b81141ea05ab76af0e7
readdan <435706956@qq.com>
2020-09-22 14:50:55 +0800

    新建项目：fruitBounty_case1

---
+++ fruitBounty_case1/assets/Script/Utils/DownloadUrl.js
@@ -0,0 +1,14 @@
+export const DownloadUrl = {
+    iosUrl: 'https://itunes.apple.com/app/id1468229094',
+    androidUrl: 'https://play.google.com/store/apps/details?id=com.bluembo.tritos&hl=en_GB',
+    
+    getUrl: function() {
+        let userAgent = navigator.userAgent || navigator.vendor;
+        if (/(iPhone|iPad|iPod|iOS)/i.test(userAgent)) {
+            userAgent = 'ios'
+        } else {
+            userAgent = 'android'
+        }
+        return userAgent === 'ios' ? this.iosUrl : this.androidUrl;
+    }
+}
