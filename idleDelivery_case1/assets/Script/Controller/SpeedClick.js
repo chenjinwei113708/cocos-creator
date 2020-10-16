d13457e33fb84bed83a6373e08322e648f612e2a
hongyuhuang <920780425@qq.com>
2020-10-16 10:30:39 +0800

    新建项目joyBlast_case1

--- idleDelivery_case1/assets/Script/Controller/SpeedClick.js
+++ idleDelivery_case1/assets/Script/Controller/SpeedClick.js
@@ -19,11 +19,25 @@ cc.Class({
 
     onLoad () {
         this.gameControl = cc.find("Canvas").getComponent('GameController');
+        this.carControl = cc.find("Canvas/center/game/rider/riderDown3").getComponent('CarController2');
+        this.vehicle = cc.find("Canvas/center/game/bottom/vehicle").getComponent('VehcleClick');
     },
 
     click(){
         if(this.gameControl.gameModel.Vehcle && this.gameControl.gameModel.PayPal){
-            console.log('1122122')
+            console.log(this.carControl)
+            let a = this.carControl.a +1;
+            let b = this.carControl.b +1;
+            this.carControl.changeSpeed(a, b);
+
+            this.vehicle.a +=1;
+            this.vehicle.b +=1;
+            this.vehicle.c +=1;
+            this.vehicle.d +=1;
+            this.vehicle.e +=1;
+            this.vehicle.f +=1;
+            this.vehicle.g +=1;
+            this.vehicle.h +=1;
         }
     },
 
