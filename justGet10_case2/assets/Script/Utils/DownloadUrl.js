cff417ee411d14f51964c28ce3e4be6c894f6dcc
readdan <435706956@qq.com>
2020-11-13 14:24:11 +0800

    新项目：justGet10_case2,已完成

---
+++ justGet10_case2/assets/Script/Utils/DownloadUrl.js
@@ -0,0 +1,14 @@
+export const DownloadUrl = {
+    iosUrl: 'https://apps.apple.com/app/id1535894969',
+    androidUrl: 'https://apps.apple.com/app/id1535894969',
+    
+    getUrl: function() {
+        let userAgent = navigator.userAgent || navigator.vendor;
+        if (/(iPhone|iPad|iPod|iOS)/i.test(userAgent)) {
+            userAgent = 'ios'
+        } else {
+            userAgent = 'android'
+        }
+        return userAgent === 'ios' ? this.iosUrl : this.androidUrl;
+    }
+}
