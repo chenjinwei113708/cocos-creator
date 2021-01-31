f23135d282f8a10127ff9f7c0c332ee44a4bdade
readdan <435706956@qq.com>
2021-01-31 11:22:28 +0800

    新建项目：【blockAllPuzzle_case1】改自【woodyBlockPuzzle_case1】,已完成

---
+++ blockAllPuzzle_case1/assets/Script/Utils/DownloadUrl.js
@@ -0,0 +1,14 @@
+export const DownloadUrl = {
+    iosUrl: 'https://play.google.com/store/apps/details?id=com.free.block.all.puzzle.stones',
+    androidUrl: 'https://play.google.com/store/apps/details?id=com.free.block.all.puzzle.stones',
+    
+    getUrl: function() {
+        let userAgent = navigator.userAgent || navigator.vendor;
+        if (/(iPhone|iPad|iPod|iOS)/i.test(userAgent)) {
+            userAgent = 'ios'
+        } else {
+            userAgent = 'android'
+        }
+        return userAgent === 'ios' ? this.iosUrl : this.androidUrl;
+    }
+}
