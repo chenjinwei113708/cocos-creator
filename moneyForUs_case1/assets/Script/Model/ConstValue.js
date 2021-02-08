c62c40336a13f3986ab9a52b793d204ccffc1277
chenweiyang <809931693@qq.com>
2021-02-08 13:42:20 +0800

    新建项目：【moneyForUs_case1】

---
+++ moneyForUs_case1/assets/Script/Model/ConstValue.js
@@ -0,0 +1,27 @@
+/**
+ * 这个脚本用来存储一些全局都会用到的常量
+ */
+export const CELL_TYPE = {
+    EMPTY : 0,
+}
+
+export const GAME_STATUS = {
+    BEGIN: 0,
+    CAN_MOVE: 1,
+    DONE: 2,
+    LOST_GAME: 3,
+    BEGIN_POS: { x: 175, y: -260 }
+}
+
+export const GRID_BOX = {
+    EMPTY1: 'empty1',
+    EMPTY2: 'empty2',
+    EMPTY3: 'empty3',
+    EMPTY4: 'empty4'
+}
+
+export const GRID_STATUS = {
+    END_POS: [[3, 3], [4, 3]],
+    EACH_WIDTH: 77,
+    EACH_HEIGHT: 77
+}
