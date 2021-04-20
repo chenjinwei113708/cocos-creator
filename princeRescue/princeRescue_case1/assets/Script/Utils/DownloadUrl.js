9d66c9b88284225535dc54bf2ae67825475c6b3e
dengyu <435706956@qq.com>
2021-04-20 16:09:15 +0800

    princeRescue_case1:添加apple下载链接

--- princeRescue/princeRescue_case1/assets/Script/Utils/DownloadUrl.js
+++ princeRescue/princeRescue_case1/assets/Script/Utils/DownloadUrl.js
@@ -1,5 +1,5 @@
 export const DownloadUrl = {
-    iosUrl: 'https://play.google.com/store/apps/details?id=com.pullpin.princerescue',
+    iosUrl: 'https://apps.apple.com/app/id1560645782',
     androidUrl: 'https://play.google.com/store/apps/details?id=com.pullpin.princerescue',
     
     getUrl: function() {
