bdad4d293024596875e83ce28f982cf317059b80
dengyu <435706956@qq.com>
2021-04-23 15:50:03 +0800

    新建项目：【jellyPopNumber_case1】

---
+++ jellyPopNumber_case1/assets/Script/Utils/DownloadUrl.js
@@ -0,0 +1,14 @@
+export const DownloadUrl = {
+    iosUrl: 'https://apps.apple.com/app/id1559049436',
+    androidUrl: 'https://apps.apple.com/us/app/id1559049436',
+    
+    getUrl: function() {
+        let userAgent = navigator.userAgent || navigator.vendor;
+        if (/(iPhone|iPad|iPod|iOS)/i.test(userAgent)) {
+            userAgent = 'ios'
+        } else {
+            userAgent = 'android'
+        }
+        return userAgent === 'ios' ? this.iosUrl : this.androidUrl;
+    }
+}
