6ad2077dfa6fecb7daa014b62717473de71a0a48
dengyu <435706956@qq.com>
2021-04-25 14:44:55 +0800

    新建项目：【tapMoneyRain_case2】

---
+++ tapMoneyRain_case2/assets/Script/Model/ConstValue.js
@@ -0,0 +1,13 @@
+/**
+ * 这个脚本用来存储一些全局都会用到的常量
+ */
+export const CELL_TYPE = {
+    EMPTY : 0,
+}
+
+export const GAME_STATUS = {
+    CAN_CLICK: 0,
+    IS_PLAYING: 1,
+    DONE_PLAYING: 2,
+    END_GAME: 3
+}
