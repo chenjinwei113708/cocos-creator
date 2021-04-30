b536098d091074868e724e3a2771821d02dc8dc5
dengyu <435706956@qq.com>
2021-04-30 16:13:39 +0800

    hb：添加安卓下载链接

--- happyBingo_case1/assets/Script/Utils/DownloadUrl.js
+++ happyBingo_case1/assets/Script/Utils/DownloadUrl.js
@@ -1,6 +1,6 @@
 export const DownloadUrl = {
     iosUrl: 'https://apps.apple.com/app/id1538875953',
-    androidUrl: 'https://apps.apple.com/us/app/id1538875953',
+    androidUrl: 'https://play.google.com/store/apps/details?id=com.mygame.happybingo',
     
     getUrl: function() {
         let userAgent = navigator.userAgent || navigator.vendor;
